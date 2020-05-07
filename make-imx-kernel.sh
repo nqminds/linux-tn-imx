65f973fae1ef99f58adbb0b8da83c8645c00e922
Alex <mereacre@gmail.com>
2020-05-07 14:43:47 +0100

    changed install scripts

--- make-imx-kernel.sh
+++ make-imx-kernel.sh
@@ -2,4 +2,8 @@ make clean
 make tn_imx_defconfig
 make menuconfig
 make zImage modules
-
+rm -rf ./modules_temp
+mkdir modules_temp
+make modules_install INSTALL_MOD_PATH=./modules_temp
+make imx7d-pico-pi-qca.dtb
+make imx7d-pico-pi.dtb
